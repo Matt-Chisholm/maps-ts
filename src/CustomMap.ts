06fb83d138e9c57fa83c00d45ed32780b756ce29
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-07-27 10:32:33 -0700

    popup window added

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,7 +1,3 @@
-import { User } from "./User";
-import { Company } from "./Company";
-
-
 // Instructions to Classes to see if they are eligible to AddMarker
 interface Mappable {
     location: {
@@ -24,13 +20,21 @@ export class CustomMap {
     }
 
     addMarker(mappable: Mappable): void {
-        new google.maps.Marker({
+        const marker = new google.maps.Marker({
             map: this.googleMap,
             position: {
                 lat: mappable.location.lat,
                 lng: mappable.location.lng
             }
         });
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: 'Hi there!'
+            });
+
+            infoWindow.open(this.googleMap, marker);
+        })
     }
 
 }
